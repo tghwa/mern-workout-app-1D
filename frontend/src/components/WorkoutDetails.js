098fa3a49152648ee070c4874bed3aca24fd4e41
tghwa <23053627@myrp.edu.sg>
2025-01-20 15:05:55 +0800

    frontend

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -7,7 +7,8 @@ const WorkoutDetails = ({ workout }) => {
   const { dispatch } = useWorkoutsContext()
 
   const handleClick = async () => {
-    const response = await fetch('/api/workouts/' + workout._id, {
+    const response = await fetch(
+      `${process.env.REACT_APP_API_URL}/api/workouts/${workout._id}`, {
       method: 'DELETE'
     })
     const json = await response.json()
