9c355825fe2b2d5cfe00f22261b17685be981d93
tghwa <23053627@myrp.edu.sg>
2025-01-20 14:54:21 +0800

    backend

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import workoutRoutes from './routes/workouts.js'; // Ensure to include the .js extension
 
+const cors = require("cors");
+
 // Express app
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
